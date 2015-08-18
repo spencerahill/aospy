f13711a02b480307aec8b4c0683e633ed3a33082
spencerahill <spencerahill@gmail.com>
2015-08-18 10:49:34 -0400

    Add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -9,5 +9,15 @@ setuptools.setup(
     description="Automated gridded climate data analysis and visualization",
     license="GPL",
     keywords="climate science",
-    url="https://github.com/spencerahill/aospy"
+    url="https://github.com/spencerahill/aospy",
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Science/Research',
+        ('License :: OSI Approved :: GNU General Public License v3 '
+         'or later (GPLv3+)'),
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2 :: Only',
+        'Topic :: Scientific/Engineering :: Atmospheric Science'
+    ]
 )
