30f38b952c7c738afc89fb45c71bea3f950efaa2
Spencer Clark <skclark@princeton.edu>
2015-09-04 09:00:47 -0400

    Removed unnecessary changes.

--- aospy/io.py
+++ aospy/io.py
@@ -4,7 +4,7 @@ import string
 import subprocess
 import numpy as np
 import netCDF4
-
+import warnings
 
 def to_dup_list(x, n, single_to_list=True):
     """
@@ -167,10 +167,7 @@ def _get_time(time, units, calendar, start_yr, end_yr, months, indices=False):
                     the time array itself at those time indices.
     :type indices: bool
     """
-    try:
-        dates = netCDF4.num2date(time[:], units, calendar.lower())
-    except:
-        dates = netCDF4.num2date(time[:], units, '360_day')    
+    dates = netCDF4.num2date(time[:], units, calendar.lower())
     inds = [i for i, date in enumerate(dates) if (date.month in months) and
             (date.year in range(start_yr, end_yr+1))]
     if indices == 'only':
