33e16b55914ed7fe2919668288af803225f6330b
Spencer Clark <skclark@princeton.edu>
2015-09-10 14:46:34 -0400

    Assigned idealized kwarg within the Run object.

--- aospy/run.py
+++ aospy/run.py
@@ -25,6 +25,7 @@ class Run(object):
         self.tags = tags
         
         self.read_mode = read_mode
+        self.idealized = idealized
         self.nc_start_day = nc_start_day
         self.nc_end_day = nc_end_day
         self.default_time_range = default_time_range 
