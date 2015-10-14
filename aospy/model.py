913e50840089159c46b60fc17eec40b8fb4a3eaf
Spencer Hill <spencerahill@gmail.com>
2015-10-14 07:38:08 -0400

    Replace netCDF4 w/ xray in Model class

--- aospy/model.py
+++ aospy/model.py
@@ -1,6 +1,6 @@
 """model.py: Model class of aospy for storing attributes of a GCM."""
 import numpy as np
-import netCDF4
+import xray
 
 from .constants import r_e
 from .utils import dict_name_keys, level_thickness
@@ -8,21 +8,19 @@ from .utils import dict_name_keys, level_thickness
 
 class Model(object):
     """Parameters of local data associated with a single climate model."""
-    def __init__(self, name='', description='', proj=False, nc_grid_paths=(),
-                 nc_dur=False, nc_start_yr=False, nc_end_yr=False,
-                 nc_start_month=False, nc_end_month=False,
-                 default_yr_range=False, runs={}, default_runs={},
-                 load_grid_data=False):
+    def __init__(self, name='', description='', proj=False, grid_file_paths=(),
+                 data_in_dur=False, data_in_start_date=False,
+                 data_in_end_date=False,
+                 default_date_range=False, runs={},
+                 default_runs={}, load_grid_data=False):
         self.name = name
         self.description = description
         self.proj = proj
-        self.nc_dur = nc_dur
-        self.nc_grid_paths = nc_grid_paths
-        self.nc_start_yr = nc_start_yr
-        self.nc_end_yr = nc_end_yr
-        self.nc_start_month = nc_start_month
-        self.nc_end_month = nc_end_month
-        self.default_yr_range = default_yr_range
+        self.data_in_dur = data_in_dur
+        self.grid_file_paths = grid_file_paths
+        self.data_in_start_date = data_in_start_date
+        self.data_in_end_date = data_in_end_date
+        self.default_date_range = default_date_range
         self.runs = dict_name_keys(runs)
         [setattr(run, 'parent', self) for run in self.runs.values()]
         if default_runs:
@@ -38,30 +36,26 @@ class Model(object):
     def __str__(self):
         return 'Model instance "' + self.name + '"'
 
-    def _get_nc_grid(self):
+    def _get_grid_files(self):
         """Get the nc_grid of an aospy object."""
-        nc_objs = []
-        for path in self.nc_grid_paths:
+        datasets = []
+        for path in self.grid_file_paths:
             try:
-                nc_obj = netCDF4.Dataset(path)
-            except TypeError:
-                nc_obj = netCDF4.MFDataset(path)
+                datasets.append(xray.open_dataset(path))
             except RuntimeError:
-                raise RuntimeError('No netCDF file found at the specified '
-                                   'directory: %s' % path)
-            nc_objs.append(nc_obj)
-        return tuple(nc_objs)
+                datasets.append(xray.open_mfdataset(path))
+        return tuple(datasets)
 
-    def _get_nc_grid_attr(self, nc_grid, attr_name):
-        """Get attribute from the nc_grid file(s)."""
-        for nc in nc_grid:
+    def _get_grid_attr(self, grid_objs, attr_name):
+        """Get attribute from the grid_objs file(s)."""
+        for xds in grid_objs:
             try:
-                return nc.variables[attr_name][:]
-            except KeyError:
+                return getattr(xds, attr_name)
+            except AttributeError:
                 pass
-        return None
+        return
 
-    def _set_mult_nc_grid_attr(self):
+    def _set_mult_grid_attr(self):
         """
         Set multiple attrs from grid file given their names in the grid file.
         """
@@ -90,24 +84,24 @@ class Model(object):
             'idim':        ('idim',),
             'fill_value':  ('fill_value')
         }
-        nc_grid = self._get_nc_grid()
+        grid_objs = self._get_grid_files()
         try:
             for name_int, names_ext in grid_attrs.iteritems():
                 for name in names_ext:
                     setattr(self, name_int,
-                            self._get_nc_grid_attr(nc_grid, name))
+                            self._get_grid_attr(grid_objs, name))
                     break
         except:
             raise
         finally:
-            for nc in nc_grid:
+            for nc in grid_objs:
                 nc.close()
 
     def grid_sfc_area(self, lonb, latb, gridcenter=False):
         """Calculate surface area of each grid cell in a lon-lat grid."""
         def diff_latlon_bnds(array):
             if array.ndim == 1:
-                return array[1:] - array[:-1]
+                return np.diff(array, axis=0)
             else:
                 return array[:,1] - array[:,0]
         dlon = diff_latlon_bnds(lonb)
@@ -147,7 +141,7 @@ class Model(object):
         """Populate the attrs that hold grid data."""
         if self.grid_data_is_set:
             return
-        self._set_mult_nc_grid_attr()
+        self._set_mult_grid_attr()
         self._set_sfc_area()
         try:
             self.levs_thick = level_thickness(self.level)
