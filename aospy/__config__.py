1bc945666ea9a12b149b1356d6d10b4cf651cacb
spencerahill <spencerahill@gmail.com>
2015-10-25 23:07:26 -0400

    Add defalt coord string labels to __config__

--- aospy/__config__.py
+++ aospy/__config__.py
@@ -4,4 +4,12 @@ default_colormap = 'RdBu'
 user_path = '/'.join([_os.getenv('HOME'),
                       'aospy_user/aospy_user']).replace('//', '/')
 
+LON_STR = 'lon'
+LAT_STR = 'lat'
+PHALF_STR = 'phalf'
+PFULL_STR = 'pfull'
+PLEVEL_STR = 'level'
+TIME_STR = 'time'
+TIME_STR_IDEALIZED = 'year'
+
 del _os
