30512b5abe3fd6be09b8eb909941e583a49b0606
spencerahill <spencerahill@gmail.com>
2015-10-26 11:18:43 -0400

    Add coordinate string constants to __init__

--- aospy/__init__.py
+++ aospy/__init__.py
@@ -1,15 +1,16 @@
 """aospy: management, analysis, and plotting of gridded climate data."""
-from .__config__ import user_path
+from .__config__ import (user_path, LAT_STR, LON_STR, PFULL_STR, PHALF_STR,
+                         PLEVEL_STR, TIME_STR)
 from . import constants
 from .constants import Constant
+from . import numerics
+from .numerics import FiniteDiff
 from . import utils
 from . import io
 from . import timedate
 from .timedate import TimeManager
 from . import units
 from .units import Units
-from . import numerics
-from .numerics import FiniteDiff
 from . import operator
 from .operator import Operator
 #from . import spharm_interface # On hold in python3
@@ -18,7 +19,7 @@ from . import var
 from .var import Var
 from . import region
 from .region import Region
-from . import run               # 'run' depends on 'time'
+from . import run
 from .run import Run
 from . import model
 from .model import Model
