506a1446057262c8590bde31a598bd21c401c45e
Spencer Clark <skclark@princeton.edu>
2016-02-07 08:13:58 -0500

    TEST Add idealized moist roundtrip test cases

--- aospy/test/test_objs/runs.py
+++ aospy/test/test_objs/runs.py
@@ -13,3 +13,16 @@ test_am2 = Run(
     data_in_end_date='0080-12-31',
     default_date_range=('0021-01-01', '0080-12-31')
 )
+
+
+test_idealized_moist = Run(
+    name='test_idealized_moist',
+    description=(
+        'Control case at T42 spectral resolution'
+    ),
+    data_in_direc='/archive/skc/idealized_moist_alb_T42/control_gaussian_T42/'
+                  'gfdl.ncrc2-default-prod/1x0m720d_32pe/history',
+    data_in_dir_struc='one_dir',
+    data_in_files={'20-day': {v: '00000.1x20days.nc'
+                              for v in ['olr', 'temp']}},
+)
--- aospy/test/test_roundtrip.py
+++ aospy/test/test_roundtrip.py
@@ -8,8 +8,8 @@ from os.path import isfile
 
 from aospy.calc import Calc, CalcInterface
 from test_objs.projects import aospy_test
-from test_objs.models import am2
-from test_objs.runs import test_am2
+from test_objs.models import am2, idealized_moist
+from test_objs.runs import test_am2, test_idealized_moist
 from test_objs.variables import olr, temp
 from test_objs.regions import nh, sahel, nh_ocean
 
@@ -20,137 +20,168 @@ skip_message = ('Model grid files cannot be located; note this '
                 'filesystems.')
 
 
-class AospyTestCase(unittest.TestCase):
+class AM2TestCase(unittest.TestCase):
     def setUp(self):
-        self.am2_olr_test_params = {'proj': aospy_test,
-                                    'model': am2,
-                                    'run': test_am2,
-                                    'var': olr,
-                                    'date_range': ('0021-01-01', '0080-12-31'),
-                                    'intvl_in': 'monthly',
-                                    'dtype_in_time': 'ts',
-                                    'dtype_in_vert': 'pressure',
-                                    'dtype_out_vert': False,
-                                    'level': False}
-        self.am2_temp_test_params = {'proj': aospy_test,
-                                     'model': am2,
-                                     'run': test_am2,
-                                     'var': temp,
-                                     'date_range': ('0021-01-01',
-                                                    '0080-12-31'),
-                                     'intvl_in': 'monthly',
-                                     'dtype_in_time': 'ts',
-                                     'level': False}
+        self.olr_test_params = {'proj': aospy_test,
+                                'model': am2,
+                                'run': test_am2,
+                                'var': olr,
+                                'date_range': ('0021-01-01', '0080-12-31'),
+                                'intvl_in': 'monthly',
+                                'dtype_in_time': 'ts',
+                                'dtype_in_vert': 'pressure',
+                                'dtype_out_vert': False,
+                                'level': False}
+        self.temp_test_params = {'proj': aospy_test,
+                                 'model': am2,
+                                 'run': test_am2,
+                                 'var': temp,
+                                 'date_range': ('0021-01-01',
+                                                '0080-12-31'),
+                                 'intvl_in': 'monthly',
+                                 'dtype_in_time': 'ts',
+                                 'level': False}
 
     def tearDown(self):
         pass
 
 
-class TestAospy(AospyTestCase):
-    @unittest.skipIf(not am2_files_exist, skip_message)
-    def test_am2_annual_mean(self):
+@unittest.skipIf(not am2_files_exist, skip_message)
+class TestAM2(AM2TestCase):
+    def test_annual_mean(self):
         calc_int = CalcInterface(intvl_out='ann',
                                  dtype_out_time='av',
-                                 **self.am2_olr_test_params)
+                                 **self.olr_test_params)
         calc = Calc(calc_int)
         calc.compute()
 
-    @unittest.skipIf(not am2_files_exist, skip_message)
-    def test_am2_annual_ts(self):
+    def test_annual_ts(self):
         calc_int = CalcInterface(intvl_out='ann',
                                  dtype_out_time='ts',
-                                 **self.am2_olr_test_params)
+                                 **self.olr_test_params)
         calc = Calc(calc_int)
         calc.compute()
 
-    @unittest.skipIf(not am2_files_exist, skip_message)
-    def test_am2_seasonal_mean(self):
+    def test_seasonal_mean(self):
         calc_int = CalcInterface(intvl_out='djf',
                                  dtype_out_time='av',
-                                 **self.am2_olr_test_params)
+                                 **self.olr_test_params)
         calc = Calc(calc_int)
         calc.compute()
 
-    @unittest.skipIf(not am2_files_exist, skip_message)
-    def test_am2_seasonal_ts(self):
+    def test_seasonal_ts(self):
         calc_int = CalcInterface(intvl_out='djf',
                                  dtype_out_time='ts',
-                                 **self.am2_olr_test_params)
+                                 **self.olr_test_params)
         calc = Calc(calc_int)
         calc.compute()
 
-    @unittest.skipIf(not am2_files_exist, skip_message)
-    def test_am2_monthly_mean(self):
+    def test_monthly_mean(self):
         calc_int = CalcInterface(intvl_out=1,
                                  dtype_out_time='av',
-                                 **self.am2_olr_test_params)
+                                 **self.olr_test_params)
         calc = Calc(calc_int)
         calc.compute()
 
-    @unittest.skipIf(not am2_files_exist, skip_message)
-    def test_am2_monthly_ts(self):
+    def test_monthly_ts(self):
         calc_int = CalcInterface(intvl_out=1,
                                  dtype_out_time='ts',
-                                 **self.am2_olr_test_params)
+                                 **self.olr_test_params)
         calc = Calc(calc_int)
         calc.compute()
 
-    @unittest.skipIf(not am2_files_exist, skip_message)
-    def test_am2_simple_reg_av(self):
+    def test_simple_reg_av(self):
         calc_int = CalcInterface(intvl_out='ann',
                                  dtype_out_time='reg.av',
                                  region={'nh': nh},
-                                 **self.am2_olr_test_params)
+                                 **self.olr_test_params)
         calc = Calc(calc_int)
         calc.compute()
 
-    @unittest.skipIf(not am2_files_exist, skip_message)
-    def test_am2_simple_reg_ts(self):
+    def test_simple_reg_ts(self):
         calc_int = CalcInterface(intvl_out='ann',
                                  dtype_out_time='reg.ts',
                                  region={'nh': nh},
-                                 **self.am2_olr_test_params)
+                                 **self.olr_test_params)
         calc = Calc(calc_int)
         calc.compute()
 
-    @unittest.skipIf(not am2_files_exist, skip_message)
-    def test_am2_complex_reg_av(self):
+    def test_complex_reg_av(self):
         calc_int = CalcInterface(intvl_out='ann',
                                  dtype_out_time='reg.av',
                                  region={'sahel': sahel},
-                                 **self.am2_olr_test_params)
+                                 **self.olr_test_params)
         calc = Calc(calc_int)
         calc.compute()
 
-    @unittest.skipIf(not am2_files_exist, skip_message)
-    def test_am2_ocean_reg_av(self):
+    def test_ocean_reg_av(self):
         calc_int = CalcInterface(intvl_out='ann',
                                  dtype_out_time='reg.av',
                                  region={'nh_ocean': nh_ocean},
-                                 **self.am2_olr_test_params)
+                                 **self.olr_test_params)
         calc = Calc(calc_int)
         calc.compute()
 
-    @unittest.skipIf(not am2_files_exist, skip_message)
-    def test_am2_vert_int_pressure(self):
+    def test_vert_int_pressure(self):
         calc_int = CalcInterface(intvl_out='ann',
                                  dtype_out_time='av',
                                  dtype_out_vert='vert_int',
                                  dtype_in_vert='pressure',
-                                 **self.am2_temp_test_params)
+                                 **self.temp_test_params)
         calc = Calc(calc_int)
         calc.compute()
 
-    @unittest.skipIf(not am2_files_exist, skip_message)
-    def test_am2_vert_int_sigma(self):
+    def test_vert_int_sigma(self):
         calc_int = CalcInterface(intvl_out='ann',
                                  dtype_out_time='av',
                                  dtype_out_vert='vert_int',
                                  dtype_in_vert='sigma',
-                                 **self.am2_temp_test_params)
+                                 **self.temp_test_params)
         calc = Calc(calc_int)
         calc.compute()
 
+
+class TestIdealized(TestAM2):
+    def setUp(self):
+        self.olr_test_params = {'proj': aospy_test,
+                                'model': idealized_moist,
+                                'run': test_idealized_moist,
+                                'var': olr,
+                                'date_range': ('0001-12-27', '0002-12-22'),
+                                'intvl_in': '20-day',
+                                'dtype_in_time': 'ts',
+                                'dtype_in_vert': 'pressure',
+                                'dtype_out_vert': False,
+                                'level': False}
+        self.temp_test_params = {'proj': aospy_test,
+                                 'model': idealized_moist,
+                                 'run': test_idealized_moist,
+                                 'var': temp,
+                                 'date_range': ('0001-12-27',
+                                                '0002-12-22'),
+                                 'intvl_in': '20-day',
+                                 'dtype_in_time': 'ts',
+                                 'level': False}
+
+    @unittest.skip('not valid in idealized moist model')
+    def test_seasonal_mean(self):
+        pass
+
+    @unittest.skip('not valid in idealized moist model')
+    def test_seasonal_ts(self):
+        pass
+
+    @unittest.skip('not valid in idealized moist model')
+    def test_monthly_mean(self):
+        pass
+
+    @unittest.skip('not valid in idealized moist model')
+    def test_monthly_ts(self):
+        pass
+
+    @unittest.skip('not valid in idealized moist model')
+    def test_vert_int_pressure(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
